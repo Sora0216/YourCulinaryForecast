d4ed06110073bcfd58d61a628234fa0aa96e85f6
EyeVorE <subplot_05tracer@icloud.com>
2024-04-02 19:18:33 -0500

    created weather function and weather dashboard and completed functionality. also created second weather function to be tied into modal.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,70 @@
-//const currentWeather = document.querySelector('')
 const recipeList = document.querySelector('ul');
 const submitButton = document.getElementById('submit-button');
 const cancelButton = document.getElementById('cancel-button');
 const getRecipesButton = document.getElementById('get-recipes-button');
 
 
-//getApi function is called when the fetchButton is clicked
-console.log(submitButton);
-
-function getWeatherApi() {
-  // Insert the API url to get a list of your repos
-  const requestWeather = 'https://api.weatherbit.io/v2.0/current?&postal_code=78704&key=b8cec87fcad4466891c1e31ddead93d0&units=I';
-
-  fetch(requestWeather)
+function requestWeather(event) {
+  event.preventDefault();
+  const zipcode = document.getElementById('zipcode-input').value
+  let weatherbitApi = `https://api.weatherbit.io/v2.0/current?&postal_code=${zipcode}&key=0ebe550f05ea43fc8fa4244dfa62a832&units=I`;
+  
+  fetch(weatherbitApi)
     .then(function (response) {
+      if (!response.ok) {
+        throw new Error('There was a problem fetching the weather data');
+      }
       return response.json();
     })
     .then(function (data) {
         console.log(data);
-    //   //looping over the fetch response and inserting the URL of your repos into a list
-    //   for (let i = 0; i < data.length; i++) {
-    //     //Create a list element
-    //     const listItem = document.createElement('li');
 
-    //     //Set the text of the list element to the JSON response's .html_url property
-    //     listItem.textContent = data[i].html_url;
-
-    //     //Append the li element to the id associated with the ul element.
-    //     repoList.appendChild(listItem);
-    //   }
+        //retrieve weather data
+        const weatherData = data.data[0];
+        //update city name
+        document.getElementById('city-name').textContent = weatherData.city_name + ',' + weatherData.state_code;
+        //update temperature
+        document.getElementById('temperature-info').textContent = weatherData.temp + '˚F';
+        //update date
+        document.getElementById('date-info').textContent = weatherData.datetime + 'pm';
+            
     });
 }
 
-function getRecipeApi() {
-    // Insert the API url to get a list of your repos
-    const requestRecipes = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=99232366887c49358403ec00d7bef302&number=5';
+
+function requestWeatherModal(event) {
+  event.preventDefault();
+  const zipcode = document.getElementById('modal-zipcode-input').value
+  let weatherbitApi = `https://api.weatherbit.io/v2.0/current?&postal_code=${zipcode}&key=0ebe550f05ea43fc8fa4244dfa62a832&units=I`;
   
-    fetch(requestRecipes)
+  fetch(weatherbitApi)
+    .then(function (response) {
+      if (!response.ok) {
+        throw new Error('There was a problem fetching the weather data');
+      }
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+
+        //retrieve weather data
+        const weatherData = data.data[0];
+        //update city name
+        document.getElementById('city-name').textContent = weatherData.city_name + ',' + weatherData.state_code;
+        //update temperature
+        document.getElementById('temperature-info').textContent = weatherData.temp + '˚F';
+        //update date
+        document.getElementById('date-info').textContent = weatherData.datetime + 'pm';
+            
+    });
+}
+
+
+
+function requestRecipes() {
+    const spoonacularAPI = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=99232366887c49358403ec00d7bef302&number=5';
+  
+    fetch(spoonacularAPI)
       .then(function (response) {
         return response.json();
       })
@@ -57,5 +85,6 @@ function getRecipeApi() {
       })
 }
 
-submitButton.addEventListener('click', getWeatherApi);
-getRecipesButton.addEventListener('click', getRecipeApi);
+submitButton.addEventListener('click', requestWeather);
+applyButton.addEventListener('click', requestWeatherModal)
+getRecipesButton.addEventListener('click', requestRecipes);
